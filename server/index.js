b4f7d161055a70eab567431ac13e5b1f8bd399c9
Jamie Pask <jamiepask1392@gmail.com>
2019-10-08 16:20:56 -0700

    Wire up mongo database

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
 // dependencies
-const express = require("express")
+const express = require('express')
 const app = express()
-const path = require("path")
+const path = require('path')
+
+const mongoose = require('mongoose')
+const companyModel = require('./mongo/companyModel')
 
 // variables
 const PORT = process.env.PORT || 3000
 
-const testData = {
-    title:"one",
-    females: 2
-}
-
-// can set basic data here -- eventually connect to a database
-app.route('/api/test')
-    .get((req, res) => {
-        res.json(testData)
-    })
+// need to authenticate user before returning data
+// TODO: move to api routes file
+app.route('/api/companyProfile').get((req, res) => {
+	mongoose
+		.connect('mongodb://localhost:27017/liveVibeDB', {
+			useNewUrlParser: true
+		})
+		.then(async () => {
+			const docs = await companyModel.findOne({companyName: "Revolution"})
+			res.json(docs)
+		})
+})
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
---
+++ server/mongo/companyModel.js
@@ -0,0 +1,108 @@
+// run 'node server/mongo/companyModel.js'
+const mongoose = require('mongoose')
+
+const companySchema = new mongoose.Schema({
+	companyName: String,
+	hashedPassword: String,
+
+	companyProfile: {
+		bio: String,
+		email: String,
+		address: {
+			propertyName: String,
+			streetName: String,
+			city: String,
+			country: String,
+			postcode: String
+        },
+        profileImage: String,
+        phoneNumber: Number,
+		openTime: Date,
+		closeTime: Date
+	},
+	countData: {
+		latestData: {
+			male: {
+				count: Number,
+				time: Date,
+				date: Date,
+				ageRange: String
+			},
+			female: {
+				count: Number,
+				time: Date,
+				date: Date,
+				ageRange: String
+			}
+		},
+		// array of object data
+		// update every 5 minutes?
+		// not accessible from user app
+		previousData: [
+			{
+				male: {
+					count: Number,
+					time: Date,
+					date: Date,
+					ageRange: String
+				},
+				female: {
+					count: Number,
+					time: Date,
+					date: Date,
+					ageRange: String
+				}
+			}
+		]
+	},
+	events: [
+		{
+			eventName: String,
+			date: Date,
+			openTime: Date, // default to standard openTime
+			closeTime: Date, // default to standard closeTime
+			eventDescription: String,
+			images: []
+		}
+	]
+})
+
+const companyModel = mongoose.model('company', companySchema, 'companies')
+
+module.exports = companyModel
+
+// export default companyModel
+
+// EXAMPLE
+// creates a new mongoDB document
+
+// let newCompany = new companyModel({
+// 	companyName: 'Revolution',
+// 	hashedPassword: 'xxxx',
+//     companyProfile: {
+// 		bio: 'Test',
+// 		email: 'Example@example.com',
+// 		address: {
+// 			propertyName: 'Example Company',
+// 			streetName: 'Example streetname',
+// 			city: 'Example city',
+// 			country: 'Example country',
+// 			postcode: 'Example postcode'
+// 		}
+// 	}
+// })
+
+// create a connection
+
+// mongoose
+// 	.connect('mongodb://localhost:27017/liveVibeDB', { useNewUrlParser: true })
+// 	.then(
+// 		() => {  
+// 			// save to database
+// 			newCompany.save(function(err) {
+// 				if (err) return handleError(err)
+// 				console.log('saved')
+// 			})
+// 		},
+// 		err => console.log(err.message)
+// 	)

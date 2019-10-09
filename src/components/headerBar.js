c349a16c29f47ca3a20df57339fb076068eb3240
Jamie Pask <jamiepask1392@gmail.com>
2019-10-09 10:58:37 -0700

    Add dark theme to headerBar

--- src/components/headerBar.js
+++ src/components/headerBar.js
@@ -1,27 +1,32 @@
 import React from 'react'
-import {
-    View,
-    Text,
-    StyleSheet
-} from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { Header } from 'react-navigation-stack'
+import { featureStyles } from '../commonStyles'
 
-const HeaderBar = props => 
-	<View style={styles.headerContainer}>
-		<Text>{props.children}</Text>
+const HeaderBar = props => (
+	<View style={props.theme === 'dark' ? styles.dark : {}}>
+		<View style={styles.headerContainer}>
+			<Text style={props.theme === 'dark' ? { color: 'white' } : {}}>
+				{props.children}
+			</Text>
+		</View>
 	</View>
+)
 
 const styles = StyleSheet.create({
 	headerContainer: {
 		height: 100,
-		justifyContent: "center",
-		alignItems: "flex-end",
+		justifyContent: 'center',
+		alignItems: 'flex-end',
 		flexDirection: 'row',
 		paddingBottom: 20,
 		marginLeft: 20,
 		marginRight: 20,
 		borderBottomWidth: 1,
-		borderBottomColor: '#E8E8E8',
+		borderBottomColor: '#E8E8E8'
+	},
+	dark: {
+		backgroundColor: featureStyles.dark
 	}
 })
 
